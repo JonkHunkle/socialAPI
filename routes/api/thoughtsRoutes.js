71d72e5388732b0524bce9aa7b4db059f0af1bd4
JonkHunkle <88416486+JonkHunkle@users.noreply.github.com>
2022-01-29 14:08:58 -0800

    just the start

---
+++ routes/api/thoughtsRoutes.js
@@ -0,0 +1,19 @@
+// ~~~/api/thoughts~~~
+
+        // get all thoughts
+        
+        //get thought by _id
+
+        //create a new thought
+                //push the created thought's `_id` to the associated user's `thoughts`
+
+        // edit a thought by _id
+
+        //delete thought by _id
+
+
+// ~~~/api/thoughts/:thoughtId/reactions~~~
+
+        // add a reaction to a single thought's `reactions` array field
+
+        //delete to remove a reaction by id
